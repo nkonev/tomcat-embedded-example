bc993dd7f7d4de9e24c069eea9448b2015ba68ed
Nikita Konev <nikit.cpp@yandex.ru>
2019-04-07 21:00:10 +0300

    savepoint tomcat 9 and manual servlet

--- src/main/java/com/example/employees/Main.java
+++ src/main/java/com/example/employees/Main.java
@@ -1,19 +1,107 @@
 package com.example.employees;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.Host;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+import org.apache.coyote.http11.Http11Nio2Protocol;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Optional;
 
 public class Main {
     public static final Optional<String> port = Optional.ofNullable(System.getenv("PORT"));
 
     public static void main(String[] args) throws Exception {
-        String contextPath = "/";
-        String appBase = ".";
+        /*String webappDirLocation = "src/main/webapp/";
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(Integer.valueOf(port.orElse("8080") ));
-        tomcat.getHost().setAppBase(appBase);
-        tomcat.addWebapp(contextPath, appBase);
+
+        //The port that we should run on can be set into an environment variable
+        //Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if(webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+
+        tomcat.setPort(Integer.valueOf(webPort));
+
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+
+        // Declare an alternative location for your "WEB-INF/classes" dir
+        // Servlet 3.0 annotation will work
+        File additionWebInfClasses = new File("target/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(), "/"));
+        ctx.setResources(resources);
+
+        tomcat.start();
+        tomcat.getServer().await();*/
+
+        Tomcat tomcat = new Tomcat();
+        tomcat.setBaseDir("temp");
+        tomcat.setPort(8080);
+
+        tomcat.setConnector(tomcat.getConnector());
+
+        String contextPath = "";
+        String docBase = new File(".").getCanonicalPath();
+
+        Context context = tomcat.addContext(contextPath, docBase);
+        //Context context = tomcat.addWebapp(contextPath, docBase);
+
+
+        HttpServlet servlet = new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+                    throws ServletException, IOException {
+                PrintWriter writer = resp.getWriter();
+
+                writer.println("<html><title>Welcome</title><body>");
+                writer.println("<h1>Have a Great Day!</h1>");
+                writer.println("</body></html>");
+            }
+        };
+
+        String servletName = "Servlet1";
+        String urlPattern = "/go";
+
+        tomcat.addServlet(contextPath, servletName, servlet);
+        context.addServletMappingDecoded(urlPattern, servletName);
+
+
+        // Declare an alternative location for your "WEB-INF/classes" dir
+        // Servlet 3.0 annotation will work
+        /*File additionWebInfClasses = new File("target/classes");
+        if (!additionWebInfClasses.exists()){
+            throw new FileNotFoundException(additionWebInfClasses.getAbsolutePath() + " not found");
+        }
+        WebResourceRoot resources = new StandardRoot(context);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(), "/"));
+        context.setResources(resources);*/
+        // Additions to make @WebServlet work
+        /*String buildPath = "target/classes";
+        String webAppMount = "/WEB-INF/classes";
+
+        File additionalWebInfClasses = new File(buildPath);
+        WebResourceRoot resources = new StandardRoot(context);
+        resources.addPreResources(new DirResourceSet(resources, webAppMount, additionalWebInfClasses.getAbsolutePath(), contextPath));
+        context.setResources(resources);*/
+        // End of additions
+
+
         tomcat.start();
         tomcat.getServer().await();
     }
